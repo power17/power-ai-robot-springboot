539fbd6e05a8a20a26c3f09080c219769ecb3858
power <liquanpower1@gmail.com>
2025-07-21 11:36:26 +0800

    build: 新增spring-ai依赖

--- src/main/java/com/power/ai/robot/PowerAiRobotSpringbootApplication.java
+++ src/main/java/com/power/ai/robot/PowerAiRobotSpringbootApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class PowerAiRobotSpringbootApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PowerAiRobotSpringbootApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(PowerAiRobotSpringbootApplication.class, args);
+    }
 
 }
