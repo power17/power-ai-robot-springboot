50c1b246dccf03374eaf4e6d306b4d66bff34b6f
power <liquanpower1@gmail.com>
2025-07-21 11:20:12 +0800

    feat: Initial commit

---
+++ src/main/java/com/power/ai/robot/PowerAiRobotSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.power.ai.robot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PowerAiRobotSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PowerAiRobotSpringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/power/ai/robot/PowerAiRobotSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.power.ai.robot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PowerAiRobotSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
