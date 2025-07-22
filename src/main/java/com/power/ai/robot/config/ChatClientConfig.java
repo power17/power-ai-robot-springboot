e2b0ccb6f7c46bf8bdd4283665e1517d4464d8aa
power <liquanpower1@gmail.com>
2025-07-22 18:15:43 +0800

    feat:新增流式输出，chatClient方式

---
+++ src/main/java/com/power/ai/robot/config/ChatClientConfig.java
@@ -0,0 +1,26 @@
+package com.power.ai.robot.config;
+
+import com.power.ai.robot.advisor.MyLoggerAdvisor;
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor;
+import org.springframework.ai.deepseek.DeepSeekChatModel;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ChatClientConfig {
+    /**
+     * 初始化 ChatClient 客户端
+     * @param chatModel
+     * @return
+     */
+    @Bean
+    public ChatClient chatClient(DeepSeekChatModel chatModel) {
+        return ChatClient.builder(chatModel)
+                .defaultSystem("请你扮演一名 Java 项目实战专栏的客服人员")
+                .defaultAdvisors(new SimpleLoggerAdvisor(), new MyLoggerAdvisor()) // 添加 Spring AI 内置的日志记录功能
+                .build();
+    }
+
+
+}
---
+++ src/main/java/com/power/ai/robot/controller/ChatClientController.java
@@ -0,0 +1,44 @@
+package com.power.ai.robot.controller;
+
+import jakarta.annotation.Resource;
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+
+@RestController
+@RequestMapping("/v2/ai")
+public class ChatClientController {
+    @Resource
+    private ChatClient chatClient;
+
+    /**
+     * 普通对话
+     *
+     * @param message
+     * @return
+     */
+    @GetMapping("/generate")
+    public String generate(@RequestParam(value = "message", defaultValue = "你是谁") String message) {
+        //一次性返回结果
+        return chatClient.prompt()
+                .user(message)
+                .call()
+                .content();
+    }
+    /**
+     * 流式对话
+     * @param message
+     * @return
+     */
+    @GetMapping(value = "/generateStream", produces = "text/html;charset=utf-8")
+    public Flux<String> generateStream(@RequestParam(value = "message", defaultValue = "你是谁？") String message) {
+        return chatClient.prompt()
+                .user(message) // 提示词
+                .stream() // 流式输出
+                .content();
+
+    }
+}
